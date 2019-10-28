cd97c81d4584bdef13a95d05e995e6a721c2c9fb
Pham Nhat Thi <phamthiruvn@gmail.com>
2019-10-29 00:27:16 +0300

    Let put on a happy face

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -396,7 +396,7 @@ fun russian(n: Int): String {
             a / 1000 in 100..999 -> {
                 result[5] = numbers3[a / 100000 - 1]
             }
-            (n / 1000) % 10 == 0 -> result[3] = "тысяч"
+            (n / 1000) % 10 == 0 && n / 1000 != 0 -> result[3] = "тысяч"
             else -> result.add("")
 
         }
