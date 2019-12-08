ecea5c19dc087a04b32cddaa465b72952d0d142d
Pham Nhat Thi <phamthiruvn@gmail.com>
2019-12-08 22:13:21 +0300

    Let put on a happy face

--- src/chipheo.kt
+++ src/chipheo.kt
@@ -7,18 +7,19 @@ import java.lang.Integer
 fun main() {
     val lol = File("input/width_in1.txt").readLines().map { it.trim() }
     val best = (lol.maxBy { it.length } ?: "").length
-    println(best)
     val outputStream = File("input/lol.txt").bufferedWriter()
-    for (line in lol)
-    {
-        val newline = line.split(" ")
-        if (newline.joinToString(" ").length == best) println( newline.joinToString(" "))
-        if (newline.size == 1 ) println(newline.joinToString(""))
-        else {
+    for (line in lol) {
 
+        val newline = line.split(" ")
+        val add = (best - newline.joinToString("").length) / newline.size
+        println(add)
+        if (line.length == best) println(line)
+        if (newline.size == 1) println(newline.joinToString(""))
+        else {
+            newline
         }
     }
-    println(" БА - БА".trim())
+
 }
 
 
