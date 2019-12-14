fd032e2d767a380952ff1dcfcd4d709c571813ee
Pham Nhat Thi <phamthiruvn@gmail.com>
2019-12-14 17:25:40 +0300

    Let put on a happy face

--- src/lastbattle.kt
+++ src/lastbattle.kt
@@ -49,9 +49,29 @@ fun main() {
         }
 
     }
-    val e = File("input/markdown_simple.md").readLines().toMutableList().map{ if (it == "") "</p><p>" else it }.joinToString("")
-println(e)
 
+    val e = File("input/markdown_simple.md").readLines().toMutableList().map { if (it == "") "</p><p>" else it }
+        .joinToString("")
+    println(e)
+    var commands = "<<<<< + >>>>>>>>>> --[<-] >+[>+] >++[--< <[<] >+[>+] >++]"
+    var duo = mutableMapOf<Int , Int>()
+    for (i in commands.indices) {
+        if (commands[i] == '[') duo[i] = 1
+        if (commands[i] == ']') duo[i] = -1
+    }
+    val keys = duo.keys.toList()
+    val vals = duo.values.toList()
+    require(vals.sum() == 0)
+    for (x in keys.indices) {
+        check(vals.take(x + 1).sum() >= 0)
+        if (vals[x] == 1) {
+            for (y in x until keys.size) if (vals.take(y + 1).drop(x).sum() == 0 && vals[y] == -1) {
+                duo[keys[x]] = keys[y]
+                duo[keys[y]] = keys[x]
+                break
+            }
+        }
+    }
+    println(duo)
 
-    "</p><p></p><p>".replace("</p><p></p><p>", "</p><p>")
 }
