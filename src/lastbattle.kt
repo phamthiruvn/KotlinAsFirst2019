b71cb113c8336334166e1226d4a15c36f56c0017
Pham Nhat Thi <phamthiruvn@gmail.com>
2020-03-03 17:26:04 +0300

    Complex + Trains

--- src/lastbattle.kt
+++ src/lastbattle.kt
@@ -4,5 +4,9 @@ import org.omg.CORBA.INTERNAL
 
 fun main() {
     val xy = mutableListOf<Int>(1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9)
-    print(xy.indexOf(-1))
+    val s = "23213-123i"
+    println(
+        Regex("""[+-]*\d*\.*\d*i*""").find(
+            s + if (!s.contains('i')) "+ 0i" else "1")!!.value.replace(Regex("""i""") , "").toDouble()
+    )
 }
