a2be68f5a694595cdb91fd8e5b57784737404e2f
Pham Nhat Thi <phamthiruvn@gmail.com>
2020-03-03 03:03:32 +0300

    11 + 12

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -18,14 +18,14 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(x, 0.0)
+    constructor(x: Double): this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(
-        Regex("""-*\d[^\D]*""").findAll(s).toList().map { it.value.toDouble() }[0],
-        Regex("""-*\d[^\D]*""").findAll(s).toList().map { it.value.toDouble() }[1]
+    constructor(s: String): this(
+        Regex("""[+-]*\d+\.*\d*i*""").findAll(s).toList().map{it.value}.map {if (it.contains('i')) 0.0 else it.toDouble()}.getOrElse(0) { 0.0 },
+        Regex("""[+-]*\d+\.*\d*i""").find(s + if (!s.contains('i')) "+ 0i" else "+ 1i" , 0)!!.value.replace(Regex("""i"""), "").toDouble()
     )
 
     /**
@@ -64,6 +64,17 @@ class Complex(val re: Double, val im: Double) {
         this === other -> true
         other is Complex -> im == other.im && re == other.re
         else -> false
+    }
+
+    /**
+     * Получение хеш-кода
+     */
+    override fun hashCode(): Int {
+        val prime = 31
+        var result = 1
+        result = prime * result + im.hashCode()
+        result = prime * result + re.hashCode()
+        return result
     }
 
     /**
--- test/lesson11/task1/ComplexTest.kt
+++ test/lesson11/task1/ComplexTest.kt
@@ -14,31 +14,53 @@ internal class ComplexTest {
     @Test
     fun plus() {
         assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("4.12-2i"), Complex("1.1+2i") + Complex("3.02-4i"), 1e-10)
+        assertApproxEquals(Complex("-4-2i"), Complex("-1+2i") + Complex("-3-4i"), 1e-10)
+        assertApproxEquals(Complex("-2i"), Complex("+2i") + Complex("-4i"), 1e-10)
+        assertApproxEquals(Complex("9-2i"), Complex("9+2i") + Complex("-4i"), 1e-10)
     }
 
     @Test
     operator fun unaryMinus() {
         assertApproxEquals(Complex(1.0, -2.0), -Complex(2.0, -1.0), 1e-10)
+        assertApproxEquals(Complex(1.99, -2.0), -Complex(2.0, -1.99), 1e-10)
     }
 
     @Test
     fun minus() {
+        assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("4-2.5i"), Complex("1+2.5i") + Complex("3-5i"), 1e-10)
         assertApproxEquals(Complex("4-2i"), Complex("1+2i") + Complex("3-4i"), 1e-10)
     }
 
     @Test
     fun times() {
         assertApproxEquals(Complex("11+2i"), Complex("1+2i") * Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("12.5"), Complex("1.5+2i") * Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("-14i"), Complex("3.5") * Complex("-4i"), 1e-10)
+        assertApproxEquals(Complex("396"), Complex("99i") * Complex("-4i"), 1e-10)
+
     }
 
     @Test
     fun div() {
         assertApproxEquals(Complex("1+2i"), Complex("11+2i") / Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("1.5+2i"), Complex("12.5") / Complex("3-4i"), 1e-10)
+        assertApproxEquals(Complex("3.5"), Complex("-14i") / Complex("-4i"), 1e-10)
+        assertApproxEquals(Complex("4i"), Complex("396") / Complex("-99i"), 1e-10)
+    }
+
+    @Test
+    fun hard() {
+        assertApproxEquals(Complex("5.3+0.9i"), (Complex("1+i") * Complex("3-2i")) + (Complex("1") / Complex("3+i")), 1e-10)
+        assertApproxEquals(Complex("-1.6+16.4i"), (Complex("3+7i") * Complex("2+i")) - (Complex("3+3i") / Complex("5")), 1e-10)
     }
 
     @Test
     fun equals() {
         assertApproxEquals(Complex(1.0, 2.0), Complex("1+2i"), 1e-12)
         assertApproxEquals(Complex(1.0, 0.0), Complex(1.0), 1e-12)
+        assertEquals(Complex(1.0, 2.0), Complex("1+2i"))
+        assertFalse(Complex(1.0, 1000.12) != Complex("1+1000.12i"))
     }
 }
